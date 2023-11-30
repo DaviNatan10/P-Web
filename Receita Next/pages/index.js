3863398fc7085342a8a1e3b59e40efafc8ab7d57
DaviNatan10 <112419442+DaviNatan10@users.noreply.github.com>
2023-11-29 22:25:14 -0300

    reorganizando

---
+++ Receita Next/pages/index.js
@@ -0,0 +1,28 @@
+export default function Home(){
+
+    return (
+
+       <div>
+
+          <h2>
+
+            Viva Santana!
+
+          </h2>
+
+       </div>
+
+    )
+
+ }
+
+ export default function Web(){
+
+    return (
+        <div>
+            <h2>
+                Testando hospedar vercel
+            </h2>
+        </div>
+    )
+ }
