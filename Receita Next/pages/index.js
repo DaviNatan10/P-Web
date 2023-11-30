3563a476460bd7af9d7b4389ffd9538e08a1d6c3
DaviNatan10 <112419442+DaviNatan10@users.noreply.github.com>
2023-11-29 22:04:32 -0300

    receita

--- Receita Next/pages/index.js
+++ Receita Next/pages/index.js
@@ -15,3 +15,38 @@ export default function Home(){
     )
  
  }
+
+ export default function Futebol(){
+
+   return (
+
+      <div>
+
+         <h2>
+
+           Flamengo É UM TIME QUE ME DECEPCIONA AS VEZES
+
+         </h2>
+
+      </div>
+
+   )
+
+}
+export default function Botafogo(){
+
+   return (
+
+      <div>
+
+         <h2>
+
+           Botafogo É UM TIME CAVALO PARAGUAIO
+
+         </h2>
+
+      </div>
+
+   )
+
+}
